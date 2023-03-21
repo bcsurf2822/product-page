31c76baaf1faae172eab900213b8ba73198a5e2c
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-03-20 23:02:31 -0400

    was able to search for products but when i try to implement the category it is returning data but not properly  need to mess with List more tm and double check the dropdown list functionality so that it is calling right category

--- src/List.js
+++ src/List.js
@@ -2,60 +2,47 @@ import { useEffect, useState } from "react";
 import Nav from "./NavBar";
 import ProductDetails from "./ProductDetail";
 
-const Page = () => {
+const Page = ({ onSearch, chooseCategory}) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
-  // const [search, setSearch] = useState("");
+  const [categories, setCategories] = useState("")
 
-  const fetchData = async (query, category, sort) => {
-    setLoading(true);
 
-    const api = "http://localhost:8000/products"
-    const parameters = {
-      query: query || undefined,
-      sort: sort || undefined,
-      category: category || undefined
-    };
-
-    api.search = new URLSearchParams(parameters).toString();
-
+  async function fetchProducts (query, category) {
     try {
-      const response = await fetch(api);
-      const data = await response.json();
-      setProducts(data.products)
-    } catch (err) {
-      console.error("Error", err);
-    } finally {
+      const filter = [];
+      if (query) filter.push(`?query=${query}`)
+      
+      if (category) filter.push(`?category=${category}`)
+      
+
+      const allFilters = filter.length > 0 ? `?${filter.join('&')}` : '';
+      const response = await fetch(`http://localhost:8000/products${allFilters}`);
+      console.log(response)
+      const productsData = await response.json();
+      const dataArray = productsData.products
+      console.log(dataArray)
+
+      setProducts(dataArray);
+      setLoading(false);
+    } catch (error) {
+      console.error("Data Error", error);
       setLoading(false);
     }
   }
 
-  // async function fetchData(query) {
-  //   try {
-  //     if (!query) {
-  //       query = "";
-  //     }
-  //     const response = await fetch(`http://localhost:8000/products${query}`);
-  //     console.log(response)
-  //     const productsData = await response.json();
-  //     const dataArray = productsData.products
-  //     console.log(dataArray)
-
-  //     setProducts(dataArray);
-  //     setLoading(false);
-  //   } catch (error) {
-  //     console.error("Data Error", error);
-  //     setLoading(false);
-  //   }
-  // }
+  const categoryFunction = (category) => {
+    setCategories(category);
+    fetchProducts("", category);
+  }
 
   useEffect(() => {
-    fetchData();
+    fetchProducts();
   }, []);
 
   return (
     <div>
-    <Nav onSearch={fetchData} />
+    <Nav onSearch={fetchProducts} chooseCategory={categoryFunction} />
     <ProductDetails products={products} load={loading} />
   </div>
   );
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,39 +1,139 @@
 import { useState } from "react";
 
-const Nav = ({onSearch}) => {
+const Nav = ({ onSearch, chooseCategory }) => {
   const [search, setSearch] = useState("");
+  const [category, setCategoies] = useState("");
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     onSearch(search);
     console.log(search);
-  }
+    setSearch("");
+  };
+
+  const handleCategory = (e) => {
+    setCategoies(e);
+    chooseCategory(e);
+  };
   return (
     <nav className="navbar bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand">Funny Store</a>
-        <div className="dropdown">
-          <button
-            className="btn btn-secondary dropdown-toggle"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Filter
-          </button>
-          <ul className="dropdown-menu">
-            <li>
-              <a className="dropdown-item" >
-                Price
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item">
-                Category
-              </a>
-            </li>
-          </ul>
-        </div>
-        <form className="d-flex" role="search">
+          <div className="dropdown">
+          <button 
+               type="button"
+               id="categoryDropdown"
+               data-bs-toggle="dropdown"
+               aria-expanded="false">{category || "Category"}</button>
+            <ul className="dropdown-menu">
+              <li>
+                <a onClick ={() => handleCategory("Animal")} className="dropdown-item">
+                  Animals
+                </a>
+              </li>
+              <li>
+                <a value="Automotive" className="dropdown-item">
+                  Automotive
+                </a>
+              </li>
+              <li>
+                <a value="Baby" className="dropdown-item">
+                  Baby
+                </a>
+              </li>
+              <li>
+                <a value="Books" className="dropdown-item">
+                  Books
+                </a>
+              </li>
+              <li>
+                <a value="Clothing" className="dropdown-item">
+                  Clothing
+                </a>
+              </li>
+              <li>
+                <a value="Computers" className="dropdown-item">
+                  Computers
+                </a>
+              </li>
+              <li>
+                <a value="Electronics" className="dropdown-item">
+                  Electronics
+                </a>
+              </li>
+              <li>
+                <a value="Garden" className="dropdown-item">
+                  Garden
+                </a>
+              </li>
+              <li>
+                <a value="Grocery" className="dropdown-item">
+                  Grocery
+                </a>
+              </li>
+              <li>
+                <a value="Health" className="dropdown-item">
+                  Health
+                </a>
+              </li>
+              <li>
+                <a value="Home" className="dropdown-item">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a value="Industrial" className="dropdown-item">
+                  Industrial
+                </a>
+              </li>
+              <li>
+                <a value="Jewelery" className="dropdown-item">
+                  Jewelery
+                </a>
+              </li>
+              <li>
+                <a value="Kids" className="dropdown-item">
+                  Kids
+                </a>
+              </li>
+              <li>
+                <a value="Movies" className="dropdown-item">
+                  Movies
+                </a>
+              </li>
+              <li>
+                <a value="Music" className="dropdown-item">
+                  Music
+                </a>
+              </li>
+              <li>
+                <a value="Outdoors" className="dropdown-item">
+                  Outdoors
+                </a>
+              </li>
+              <li>
+                <a value="Shoes" className="dropdown-item">
+                  Shoes
+                </a>
+              </li>
+              <li>
+                <a value="Sports" className="dropdown-item">
+                  Sports
+                </a>
+              </li>
+              <li>
+                <a value="Tools" className="dropdown-item">
+                  Tools
+                </a>
+              </li>
+              <li>
+                <a value="Toys" className="dropdown-item">
+                  Toys
+                </a>
+              </li>
+            </ul>
+          </div>
+        <form onSubmit={handleSubmit} className="d-flex" role="search">
           <input
             className="form-control me-2"
             type="text"
@@ -41,7 +141,7 @@ const Nav = ({onSearch}) => {
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           ></input>
-          <button className="btn btn-outline-primary" onClick={handleSubmit}>
+          <button type="submit" className="btn btn-outline-primary">
             Search
           </button>
         </form>
